081b023d8d329b26165a6417305f54a0fca8b4df
fpdjsns.github.io <withham1@naver.com>
2018-07-17 20:44:56 +0900

    change path '/' to '/member'

--- src/main/java/com/apitest/demo/GetTest.java
+++ src/main/java/com/apitest/demo/GetTest.java
@@ -7,21 +7,22 @@ import org.springframework.web.bind.annotation.*;
 import java.lang.reflect.Member;
 
 @RestController
+@RequestMapping("/member")
 public class GetTest {
 
-    // localhost:8080/ok
+    // localhost:8080/member/ok
     @RequestMapping("/ok")
     public ResponseEntity<String> root(){
         return ResponseEntity.ok("Get OK");
     }
 
-    // localhost:8080?member=testMember
+    // localhost:8080/member?member=testMember
     @RequestMapping("/")
     public ResponseEntity<String> requestParamTest(@RequestParam(value = "member") String member){
         return ResponseEntity.ok(member);
     }
 
-    // localhost:8080/testMember
+    // localhost:8080/member/testMember
     @GetMapping("/{member}")
     public ResponseEntity<MemberApiResponse> pathVariableTest(@PathVariable String member){
         MemberApiResponse response = new MemberApiResponse();
